fe12d19bb25919b7b854680fffbfc538d13187b4
Brennan MacKay <brennanmackay@gmail.com>
2021-12-02 11:01:58 -0400

    Day 2

--- cmd/aoc-2021/main.go
+++ cmd/aoc-2021/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/BrennanMacKay/aoc-2021/internal/d0"
 	"github.com/BrennanMacKay/aoc-2021/internal/d1"
+	"github.com/BrennanMacKay/aoc-2021/internal/d2"
 	"os"
 )
 
@@ -18,6 +19,8 @@ func main() {
 		os.Exit(d0.Day0(os.Args[2:]))
 	case "d1":
 		os.Exit(d1.Day1(os.Args[2:]))
+	case "d2":
+		os.Exit(d2.Day2(os.Args[2:]))
 	default:
 		fmt.Printf("%s did not match a known problem\n", os.Args[1])
 		os.Exit(1)
